3809b967d29983baa1222c326c585008f8b89ce2
chenfei <chenfei@2xi.com>
2018-09-05 22:48:08 +0800

    首页title修改

--- Test/MainTitleView.js
+++ Test/MainTitleView.js
@@ -5,21 +5,20 @@ import {
     Text,
     View,
     Dimensions,
-    Button,
     Image,
+    Button,
     TouchableOpacity
 } from 'react-native';
-
 const { kwidth , kheight} = Dimensions.get('window');
 export default class MainTitleView extends Component{
     render(){
         return(
             <View style={styles.container}>
-                <TouchableOpacity onPress={this.props.scan()}>
+                <TouchableOpacity onPress={this.props.scan}>
                     <Image source={require('../resources/in-scan-code.png')} style={styles.leftStyle} />
                 </TouchableOpacity>
 
-                <Button onPress={this.props.exchange()}
+                <Button onPress={this.props.exchange}
                     title='生活家'
                     color="#333333"
                     style={styles.bttonStyle}
@@ -38,6 +37,8 @@ const styles = StyleSheet.create({
     },
     leftStyle:{
         marginLeft:15,
+        marginTop:31,
+        marginBottom:'auto',
         alignItems:'center',
         justifyContent:'center',
         alignSelf:'center',
@@ -45,9 +46,14 @@ const styles = StyleSheet.create({
         height:22
     },
     bttonStyle:{
-        alignItems:'center',
-        justifyContent:'center',
-        alignSelf:'center',
+        flex:1,
+        marginLeft:280,
+        marginRight:'auto',
+        marginTop:31,
+        height:33,
+        backgroundColor:'#2bb2c1',
+         alignItems:'center',
+        // justifyContent:'center',
     },
 
 });
