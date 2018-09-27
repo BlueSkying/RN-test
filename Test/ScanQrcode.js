ea04067497603047e09bd7b3e941a03401ded6d8
chenfei <chenfei@2xi.com>
2018-09-27 21:17:03 +0800

    merge master

--- Test/ScanQrcode.js
+++ Test/ScanQrcode.js
@@ -1,102 +1,180 @@
-import React, { Component } from 'react';
+import React from 'react'
 import {
-    AppRegistry,
     StyleSheet,
-    Text,
-    View,
-    Image,
-    Dimensions,
     TouchableOpacity,
-    // AsyncStorage,
-    TextInput,
-} from 'react-native';
-/*
- import Storage from 'react-native-storage';
-var storge = new Storage({
-    size:1000,
-    storageBackend:AsyncStorage,
-    defaultExpires:null,
-    enableCache:null,
-})
-global.storage = storge
-*/
-export const kwidth = Dimensions.get('window').width;
-// 获取设备屏幕高
-export const kheight = Dimensions.get('window').height;
+    View,
+    Text,
+    InteractionManager,
+    Animated,
+    Easing,
+    Platform,
+    Image,
+    ImageBackground,
+    Dimensions,
+}from 'react-native';
+import Camera from 'react-native-camera';
+var Dimensions = require('Dimensions');
+var {width, height} = Dimensions.get('window');
 
-export default class LoginVCN extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            nameText:'请输入用户账号',
-            passText:'请输入用户密码',
-            dataList: this.loadFromLocal(),
+export default class ScanQrcode extends React.Component{
+     constructor(props){
+         super(props);
+         this.camera = null;
+         this.state = {
+             show:true,
+             anim: new Animated.Value(0),
+             camera: {
+                 aspect: Camera.constants.Aspect.fill,
+             },
+         };
+     }
+    componentDidMount(){
+        InteractionManager.runAfterInteractions(()=>{
+            this.startAnimation()
+        });
+    }
+    startAnimation(){
+        if(this.state.show){
+            this.state.anim.setValue(0)
+            Animated.timing(this.state.anim,{
+                toValue:1,
+                duration:1500,
+                easing:Easing.linear,
+            }).start(()=>this.startAnimation());
         }
     }
-    /*
-        componentDidMount() {
-            global.storage.save({
-                key:'token',
-                data:'data.token',  //取回来存储的数据
-                expires:null
-            })
-
-            let token = global.storage.load({   //取存储的数据
-                key:'token'
-            })
+    componentWillUnmount(){
+        this.state.show = false;
+    }
+    //扫描二维码结果返回方法
+    barcodeReceived = (e) =>{
+        if(this.state.show){
+            this.state.show = false;
+            if (e) {
+                const { goBack } = this.props.navigation;
+                goBack();
+                if(this.props.navigation.state.params.callback){
+                    this.props.navigation.state.params.callback(e.data)
+                }
+                // this.props.ReceiveCode(e.data)
+            } else {
+                Alert.alert(
+                    '提示',
+                    '扫描失败'
+                        [{text:'确定'}]
+                )
+            }
         }
-
-        //加载本地缓存数据
-        loadFromLocal = async()=>{
-            let list = await  storge.load({
-                key:'recommendList'
-            })
-            this.setState({
-                dataList:list
-            })
-        }
-    */
-    //关闭本页面
-    closeLogin = ()=>{
-
     }
 
-    componentWillUnmount() {
+    render() {
+        return (
+            <View style={styles.container}>
+                {/*导航条*/}
+                {this.renderNavBar()}
+                <Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    style={styles.preview}
+                    aspect={this.state.camera.aspect}
+                    onBarCodeRead={this.barcodeReceived.bind(this)}
+                    // barCodeTypes = {['qr']}
 
+                >
+                    <View style = {{height: Platform.OS == 'ios' ? (height-264)/3:(height-244)/3,width:width,backgroundColor:'rgba(0,0,0,0.5)',}}>
+                    </View>
+                    <View style={{flexDirection:'row'}}>
+                        <View style={styles.itemStyle}/>  //扫描框左边空白
+                        <ImageBackground style={styles.rectangle}  //扫描背景边框
+                                source={require('../resources/qr_scan_line.png')}>
+                            <Animated.View style={[styles.animatiedStyle, {     //扫描的上下滚动条
+                                transform: [{
+                                    translateY: this.state.anim.interpolate({
+                                        inputRange: [0,1],
+                                        outputRange: [0,200]
+                                    })
+                                }]
+                            }]}>
+                            </Animated.View>
+                        </ImageBackground>
+                        <View style={styles.itemStyle}/>   //扫描框右边空白
+                    </View>
+                    <View style={{flex:1,backgroundColor:'rgba(0, 0, 0, 0.5)',width:width,alignItems:'center'}}>
+                        <Text style={styles.textStyle}>将二维码放入框内,即可自动扫描</Text>
+                    </View>
+                </Camera>
+            </View>
+        );
     }
 
-    render(){
+
+    // 导航条
+    renderNavBar(){
         return(
-            <View size={styles.container}>
-                <TouchableOpacity onPress={this.closeLogin()}>
-                    <Image style={{width:40,height:40,marginLeft:15,marginTop:15}}
-                           source={{uri:'../resources/close.png'}}
-                    />
+            <View style={styles.navBarStyle}>
+                <TouchableOpacity
+                    onPress={()=>{this.props.navigator.pop()}}
+                    style={styles.leftViewStyle}>
+                    <Image source={{uri:'nav_return'}}
+                           style={{height:20,width:20}} />
                 </TouchableOpacity>
-                <View style={{width:kwidth,height:100,justifyContent:'center',alignItems:'center'}}>
-                    <Image style={{width:80,height:80,marginTop:10,marginLeft:'auto',marginRight:'auto'}}
-                           source={{uri:'../resources/login_logo_icon.png'}}
-                    />
-                </View>
-                <View>
-                    <Text>
-                        用户名：
-                    </Text>
-                    <TextInput style={{height:40,borderColor:'gray',borderWidth:1}}
-                               onChangeText={(text)=>this.setState({nameText})}
-                               value={this.state.nameText}
-                               defaultValue={'请输入用户账号'}
-                               clearButtonMode={'while-editing'}
-                    />
-                </View>
-
+                <Text style={[styles.navTitleStyle,{marginTop:Platform.OS == 'ios'?12:0,fontSize:20}]}>
+                    二维码
+                </Text>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex:1
+    itemStyle:{
+        backgroundColor:'rgba(0,0,0,0.5)',
+        width:(width-200)/2,
+        height:200
+    },
+    textStyle:{
+        color:'#fff',
+        marginTop:10,
+        fontWeight:'bold',
+        fontSize:18
+    },
+    navTitleStyle: {
+        color:'white',
+        fontWeight:'bold',
+    },
+    navBarStyle:{ // 导航条样式
+        height: Platform.OS == 'ios' ? 64 : 44,
+        backgroundColor:'rgba(34,110,184,1.0)',
+        // 设置主轴的方向
+        flexDirection:'row',
+        // 垂直居中 ---> 设置侧轴的对齐方式
+        alignItems:'center',
+        justifyContent:'center'
+    },
+
+    leftViewStyle:{
+        // 绝对定位
+        // 设置主轴的方向
+        flexDirection:'row',
+        position:'absolute',
+        left:10,
+        bottom:Platform.OS == 'ios' ? 15:12,
+        alignItems:'center',
+        width:30
+    },
+    animatiedStyle:{
+        height:2,
+        backgroundColor:'#00FF00'
+    },
+    container: {
+        flex: 1,
+    },
+    preview: {
+        flex: 1,
+    },
+    rectangle: {
+        height: 200,
+        width: 200,
     }
-})
+});
