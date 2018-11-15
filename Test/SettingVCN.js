fbbe2f2e71ba0ddf229b3ffd925e5fa4c71bfe1f
huangchen <huangchen@huangchendeMac-mini.local>
2018-11-15 18:25:00 +0800

    公司电脑热更新成功

--- Test/SettingVCN.js
+++ Test/SettingVCN.js
@@ -7,7 +7,23 @@ import {
     Dimensions,
     TouchableOpacity,
     DeviceEventEmitter,
+    Platform,
+    Alert,
 } from 'react-native';
+import {
+    isFirstTime,
+    isRolledBack,
+    packageVersion,
+    currentVersion,
+    checkUpdate,
+    downloadUpdate,
+    switchVersion,
+    switchVersionLater,
+    markSuccess,
+} from 'react-native-update';
+
+import _updateConfig from '../update.json';
+const {appKey} = _updateConfig[Platform.OS];
 
 export const kwidth = Dimensions.get('window').width;
 // 获取设备屏幕高
@@ -28,6 +44,46 @@ export default class SettingVCN extends Component{
 
         }
     }
+
+    componentWillMount(){
+        if (isFirstTime) {
+            Alert.alert('提示', '这是当前版本第一次启动,是否要模拟启动失败?失败将回滚到上一版本', [
+                {text: '是', onPress: ()=>{throw new Error('模拟启动失败,请重启应用')}},
+                {text: '否', onPress: ()=>{markSuccess()}},
+            ]);
+        } else if (isRolledBack) {
+            Alert.alert('提示', '刚刚更新失败了,版本被回滚.');
+        }
+    }
+    doUpdate = info => {
+        downloadUpdate(info).then(hash => {
+            Alert.alert('提示', '下载完毕,是否重启应用?', [
+                {text: '是', onPress: ()=>{switchVersion(hash);}},
+                {text: '否',},
+                {text: '下次启动时', onPress: ()=>{switchVersionLater(hash);}},
+            ]);
+        }).catch(err => {
+            Alert.alert('提示', '更新失败.');
+        });
+    };
+    checkUpdate = () => {
+        checkUpdate(appKey).then(info => {
+            if (info.expired) {
+                Alert.alert('提示', '您的应用版本已更新,请前往应用商店下载新的版本', [
+                    {text: '确定', onPress: ()=>{info.downloadUrl && Linking.openURL(info.downloadUrl)}},
+                ]);
+            } else if (info.upToDate) {
+                Alert.alert('提示', '您的应用版本已是最新.');
+            } else {
+                Alert.alert('提示', '检查到新的版本'+info.name+',是否下载?\n'+ info.description, [
+                    {text: '是', onPress: ()=>{this.doUpdate(info)}},
+                    {text: '否',},
+                ]);
+            }
+        }).catch(err => {
+            Alert.alert('提示', '更新失败.');
+        });
+    };
 
     componentDidMount() {
         this.props.navigation.setParams({
@@ -65,10 +121,19 @@ export default class SettingVCN extends Component{
     render(){
         return(
             <View style={{marginTop:80}}>
+                <TouchableOpacity onPress={this.checkUpdate}>
+                    <View style={styles.btnBgStyle}>
+                        <Text style={styles.loginBtnStyel}>
+                            检查更新
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+                <View style={{width:kwidth,height:40}}>
+                </View>
                 <TouchableOpacity onPress={this.LogOut}>
                     <View style={styles.btnBgStyle}>
                         <Text style={styles.loginBtnStyel}>
-                                    退出登录
+                            退出登录
                         </Text>
                     </View>
                 </TouchableOpacity>
