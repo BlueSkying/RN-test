db9290177cea85dd417a80335cfc29b50a8e62a5
huangchen <huangchen@huangchendeMac-mini.local>
2018-11-20 17:46:09 +0800

    mobx使用成功

--- Test/ResidentVCN.js
+++ Test/ResidentVCN.js
@@ -11,14 +11,19 @@ import {
     ImageBackground,
 } from 'react-native';
 import {observable,action} from 'mobx';
-import {observe} from 'mobx-react/native';
+import {observer} from 'mobx-react/native';
 import Request from './Request';
 import Config from './config';
 export const kwidth = Dimensions.get('window').width;
 // 获取设备屏幕高
 export const kheight = Dimensions.get('window').height;
+var count = 1;
 
+@observer
 export default class ResidentVCN extends Component{
+
+    @observable dataSource = [];
+
     static navigationOptions = ({navigation, screenProps}) => ({
         headerTitle: "小区公告",
         headerTitleStyle:{flex:1,textAlign:'center',alignSelf:'center'},
@@ -27,14 +32,13 @@ export default class ResidentVCN extends Component{
 
     constructor(props){
         super(props)
-        let projectID = '1562561263';
+        let projectID = '38562569';
         let userID = '1285858633';
-        var dataSource = [];
-        var count = 1;
         this.state={
             projectID:projectID,
             userID:userID,
-            dataSource:dataSource,
+            isRefresh:false,
+            isLoadMore:false
         }
     }
 
@@ -44,10 +48,16 @@ export default class ResidentVCN extends Component{
 
     loadResident = ()=>{
         Request.post(Config.api.residentListUrl,{'params':{'contactId':this.state.userID,'projectId':this.state.projectID,
-            'contentType':'1','classId':'villageNotice','pageSize':'20','page':count}},(data)=>{
-            this.setState({
-                dataSource : data["data"]["list"]
-            })
+            'contentType':'1','classId':'villageNotice','pageSize':'20','page':String(count),'title':'','type':'1'}},(data)=>{
+            if(count === 1){
+                this.dataSource = data["data"]["list"]
+            }else{
+                this.dataSource = this.dataSource.concat( data["data"]["list"])
+                this.setState({
+                    isLoadMore:false,
+                })
+            }
+
         },(error)=>{
             console.warn(error);
         })
@@ -55,41 +65,48 @@ export default class ResidentVCN extends Component{
 
     _renderItem = (item) => {
         return(
-            <View style={{flex:1,flexDirection:'row'}}>
-                <Image source={{uri:item.picUrl}} style={styles.IconImgStyle}/>
-                <View style={{flexDirection:'column'}}>
-                    <Text>{item.title}</Text>
-                    <Text>{item.classname}</Text>
-                    <Text>{item.createTime}</Text>
+            <View style={{flex:1,flexDirection:'row',width:kwidth,height:110}}>
+                <Image source={{uri:item.item.picUrl}} style={styles.IconImgStyle}/>
+                <View style={{flexDirection:'column',marginLeft:15}}>
+                    <Text style={styles.titleStyle}>{item.item.title}</Text>
+                    <View style={{flexDirection:'row'}}>
+                        <Text style={styles.timeStyel}>{item.item.createTime}</Text>
+                        <Text style={styles.timeStyel}>{item.item.classname}</Text>
+                    </View>
                 </View>
             </View>
         )
     }
-
+    @action
     refreshing(){
-        let timer =  setTimeout(()=>{
-            clearTimeout(timer)
-            alert('刷新成功')
-        },1500)
+        if(!this.state.isRefresh){
+            count = 1
+            this.loadResident()
+        }
     }
-
+    @action
     _onload(){
-        let timer =  setTimeout(()=>{
-            clearTimeout(timer)
-            alert('加载成功')
-        },1500)
+        if(!this.state.isLoadMore && this.dataSource.length>0){
+            count ++
+            this.loadResident()
+        }
     }
 
+    _separator = () => {
+        return <View style={{height:5,backgroundColor:'white'}}/>;
+    }
+    @observer
     render(){
         return(
             <View style={{flex:1}}>
                 <FlatList
-                    onRefresh={this.refreshing}
+                    onRefresh={()=>this.refreshing()}
                     refreshing={false}
                     onEndReached={
-                        this._onload
+                        ()=>this._onload()
                     }
-                    data={dataSource}
+                    ItemSeparatorComponent={this._separator}
+                    data={this.dataSource}
                     renderItem={this._renderItem}
                 >
                 </FlatList>
@@ -104,5 +121,19 @@ const styles = StyleSheet.create({
        marginTop:15,
        width:80,
        height:80,
-   }
+   },
+    titleStyle:{
+        fontSize:14,
+        marginLeft:15,
+        marginTop:15,
+        width:kwidth-80-45,
+        height:40,
+    },
+    timeStyel:{
+        fontSize:12,
+        marginLeft:15,
+        marginTop:0,
+        width:155,
+        height:20,
+    }
 });
